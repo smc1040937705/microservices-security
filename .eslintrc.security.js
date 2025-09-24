16442fedc99ba084bd6595d1865a568de2c1048f
smc1040937705 <76459263+smc1040937705@users.noreply.github.com>
2025-09-24 11:30:32 +0800

    feat: add ESLint security configuration for code analysis

---
+++ .eslintrc.security.js
@@ -0,0 +1,26 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  plugins: ['security'],
+  extends: [
+    'plugin:security/recommended'
+  ],
+  rules: {
+    'security/detect-buffer-noassert': 'error',
+    'security/detect-child-process': 'error',
+    'security/detect-disable-mustache-escape': 'error',
+    'security/detect-eval-with-expression': 'error',
+    'security/detect-new-buffer': 'error',
+    'security/detect-no-csrf-before-method-override': 'error',
+    'security/detect-non-literal-fs-filename': 'error',
+    'security/detect-non-literal-regexp': 'error',
+    'security/detect-non-literal-require': 'error',
+    'security/detect-object-injection': 'error',
+    'security/detect-possible-timing-attacks': 'error',
+    'security/detect-pseudoRandomBytes': 'error',
+    'security/detect-unsafe-regex': 'error'
+  },
+  env: {
+    node: true,
+    es6: true
+  }
+};
